e299e71f32808e98a741a1ae38eaa579c2813bd9
jatinbindal <jatinbindal023@gmail.com>
2020-03-02 00:29:26 +0530

    SearchInRotatedSortedArray

---
+++ LeetCode/src/test/java/SearchInRotatedSortedArrayTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SearchInRotatedSortedArrayTest {
+
+    @Test
+    public void testSearchInRotatedArray() {
+        Assert.assertEquals(SearchInRotatedSortedArray.search(new int[]{3,1},0),-1);
+
+
+    }
+
+
+}
