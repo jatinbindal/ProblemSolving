58480657362f9cf4fde4b6f668331d88181638a9
jatinbindal <jatinbindal023@gmail.com>
2020-03-14 20:29:09 +0530

    "Sort Color : Dutch Partitoining Algo"

---
+++ LeetCode/src/main/java/SortColors.java
@@ -0,0 +1,23 @@
+public class SortColors {
+    public void sortColors(int[] nums) {
+        //Dutch partioning algorithm
+        int low=0,reader=0,high=nums.length-1;
+
+        while(low<=high){
+            if(nums[low]==0){
+                nums[low] = nums[reader];
+                nums[reader] = 0;
+                low++;
+                reader++;
+            }
+            else if(nums[low]==1) low++;
+            else{
+                nums[low]=nums[high];
+                nums[high]=2;
+                high--;
+
+            }
+        }
+    }
+
+}
