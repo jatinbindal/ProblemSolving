d653873c98867cdc81851813d8aa7b9a2f560987
jatin <jatinbindal023@gmail.com>
2020-09-04 21:41:12 +0530

    ContainsDup3 good enough

---
+++ LeetCode/src/main/java/ContainsDuplicate3.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+class Solution {
+        public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+            if(nums == null || nums.length == 0) return false;
+
+            HashMap<Long,Long> buckets = new HashMap<>();
+            int i = 0;
+            long tLong = (long)t;
+            for(long n : nums){
+                long bucket = n/(tLong+1);
+                if(n<0) bucket--;
+
+                if(buckets.containsKey(bucket))
+                    return true;
+
+                Long left = buckets.get(bucket-1);
+                if(left!=null && n-left<=tLong)
+                    return true;
+
+                Long right = buckets.get(bucket+1);
+                if(right!=null && right-n <=tLong)
+                    return true;
+
+                buckets.put(bucket,n);
+                if(buckets.size()>k)
+                    buckets.remove(nums[i-k]/(tLong+1));
+                i++;
+
+
+            }
+
+            return false;
+
+        }
+    }
+
