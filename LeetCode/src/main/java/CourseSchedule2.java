69be6e73b18ca8062b6fc89aca74ee65798aaad7
jatin <jatinbindal023@gmail.com>
2020-10-14 23:15:39 +0530

    course schedule 2

--- LeetCode/src/main/java/CourseSchedule2.java
+++ LeetCode/src/main/java/CourseSchedule2.java
@@ -5,44 +5,48 @@ import java.util.ArrayList;
 //Scheduling problem
 class CourseSchedule2 {
 
-    int ind = 0;
-    public int[] findOrder(int n, int[][] p) {
 
-        int[] result = new int[n];
+        int ind = 0;
+        public int[] findOrder(int n, int[][] p) {
 
-        ArrayList<Integer>[] map  =  new ArrayList[n];
-        for(int i = 0 ; i < n ; i++)
-            map[i] = new ArrayList<>();
+            int[] result = new int[n];
 
-        for(int[] row : p){
-            map[row[0]].add(row[1]);
-        }
+            ArrayList<Integer>[] map  =  new ArrayList[n];
+            for(int i = 0 ; i < n ; i++)
+                map[i] = new ArrayList<>();
 
-
-        int[] visited  = new int[n];
-        for(int i = 0;i < n ;  i++){
-            if(visited[i]==2) continue;
-            if(topologicalIsCyclic(i,map,visited,result))
-                return new int[0];
-        }
-
-        return  result;
-    }
-
-    public boolean topologicalIsCyclic(int n, ArrayList<Integer>[] map , int[] v,int[] result){
-
-        if(v[n]==1) return true ;
-        if(v[n]==2) return false;
-        v[n] = 1;
-        Pi        if(v[n]==2) continue;
-                if(topologicalIsCyclic(i,map,v,result))
-                    return true;
+            for(int[] row : p){
+                map[row[0]].add(row[1]);
             }
+
+
+            int[] visited  = new int[n];
+            for(int i = 0;i < n ;  i++){
+                if(visited[i]==2) continue;
+                if(topologicalIsCyclic(i,map,visited,result))
+                    return new int[0];
+            }
+
+            return  result;
         }
 
-        v[n] = 2;
-        result[ind++] = n;
-        return false;
+        public boolean topologicalIsCyclic(int n, ArrayList<Integer>[] map , int[] v,int[] result){
 
+            if(v[n]==1) return true ;
+            if(v[n]==2) return false;
+            v[n] = 1;
+            ArrayList<Integer> child = map[n];
+            if(child!=null) {
+                for(int i : child){
+                    if(v[n]==2) continue;
+                    if(topologicalIsCyclic(i,map,v,result))
+                        return true;
+                }
+            }
+
+            v[n] = 2;
+            result[ind++] = n;
+            return false;
+
+        }
     }
-}
